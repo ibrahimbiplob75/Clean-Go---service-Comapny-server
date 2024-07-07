f64b3e9b6da5f35afc5bfcb5d45eea2373b1adfa
ibrahim75 <ibrahimbiplob75@gmail.com>
2024-07-07 14:53:36 +0600

    Server operation done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors=require("cors")
 const app=express();
 require('dotenv').config();
 var jwt = require('jsonwebtoken');
+var cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 3000;
@@ -10,8 +11,27 @@ const port = process.env.PORT || 3000;
 
 //middleware
 app.use(cors());
+
+const verified=async(req,res,next)=>{
+  const token=req.cookies?.token
+  
+  if(!token){
+    return res.status(401).send({ message: 'unauthorized access no token' })
+  }
+  jwt.verify(token,process.env.SECRET_TOKEN,(error,decode)=>{
+    if(error){
+      return res.status(401).send({ message: 'unauthorized access no token' })
+    }
+    // console.log(decode)
+    req.user=decode
+    next();
+  })
+  
+}
+
 //parser
 app.use(express.json());
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.ij6ptye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -23,6 +43,8 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
 async function run() {
   try {
     await client.connect();
@@ -32,28 +54,25 @@ async function run() {
     app.get("/api/user/access-token",async(req,res)=>{
         const user=req.body
         const token=jwt.sign(user,process.env.SECRET_TOKEN,{expiresIn:60*60})
-        console.log(token)
+        res.cookie("token",token,{
+            httpOnly:true,
+            secure:false,
+            sameSite:"none"
+        }).send({"Success":true})
     })
 
-    app.get("/api/user/services",async(req,res)=>{
+    app.get("/api/user/services",verified,async(req,res)=>{
         const result=await services.find().toArray()
         res.send(result)
     })
 
+    
+
     app.post("/api/user/create-booking",async(req,res)=>{
         const booking=req.body
-        
         const result=await bookings.insertOne(booking)
         res.send(result)
 
-//         {
-//     "booked-name":"Md Ibrahim",
-//     "booked-email":"ibrahimbiplob75@gmail.com",
-//     "booked-phone":"015111111",
-//     "booked-service":"House clean",
-//     "booked-price":"2000",
-//     "booked-date":"21-07-24"
-// }
     })
 
     app.delete("/api/user/cancel-booking/:id",async(req,res)=>{
@@ -61,6 +80,25 @@ async function run() {
         const query={_id:new ObjectId(id)}
         const result=await bookings.deleteOne(query)
         res.send(result)
+    })
+
+    app.get("/api/user/bookings",verified,async(req,res)=>{
+      const queryEmail=req.query?.email
+      const tokenEmail=req.user?.email
+      if(queryEmail!==tokenEmail){
+        return res.status(403).send({"message":"Forbidden acccess"})
+      }
+
+      let query={}
+      if(queryEmail){
+        query.email=queryEmail
+        
+      }
+
+      const result=await bookings.find(query).toArray()
+      res.send(result)
+      
+
     })
     
     
